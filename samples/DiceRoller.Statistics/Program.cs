2429838526fb221289fd20f38e6b74a2c8f23a21
Erik Lindström <erik.lindstrom@zapote.se>
2017-09-12 23:05:37 +0200

    dunno

--- samples/DiceRoller.Statistics/Program.cs
+++ samples/DiceRoller.Statistics/Program.cs
@@ -9,7 +9,6 @@ namespace DiceRoller.Statistics
         {
             Console.Title = "DiceRoller Statistics";
             Bus.Configure().UseRabbitMQ();
-
             Bus.Subscribe("diceroller.service");
         }
     }
