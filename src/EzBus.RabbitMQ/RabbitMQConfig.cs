72a2236ed1543ab7aa0abd793565c927f280b5d3
Peter <peter.johansson@zapote.se>
2020-02-07 18:06:27 +0100

    use envvars for rabbit mq broker con

--- src/EzBus.RabbitMQ/RabbitMQConfig.cs
+++ src/EzBus.RabbitMQ/RabbitMQConfig.cs
@@ -1,4 +1,5 @@
 using System;
+using Environment;
 
 namespace EzBus.RabbitMQ
 {
@@ -10,9 +11,9 @@ namespace EzBus.RabbitMQ
         public ushort RequestedHeartbeat { get; set; } = 5;
         public ushort PrefetchCount { get; set; } = 100;
         public string ExchangeType { get; set; } = RabbitMQ.ExchangeType.Fanout;
-        public string UserName { get; set; } = "guest";
-        public string Password { get; set; } = "guest";
-        public string HostName { get; set; } = "localhost";
+        public string UserName { get; set; } = Environment.GetEnvironmentVariable("rabbit_username") ?? "guest";
+        public string Password { get; set; } = Environment.GetEnvironmentVariable("rabbit_password") ?? "guest";
+        public string HostName { get; set; } = Environment.GetEnvironmentVariable("rabbit_hostname") ?? "localhost";
         public string VirtualHost { get; set; } = "/";
     }
 }
