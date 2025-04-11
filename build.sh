6b03d2f5d5fa795528d06a91e7a1debbe389827e
Erik Lindström <erik.lindstrom@zapote.se>
2025-04-11 14:56:35 +0200

    packaging

---
+++ build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# This script is used to build the EzBus project and run tests.
+# It assumes that the .NET SDK is installed and available in the PATH.
+# Usage: ./build.sh
+# Check if .NET SDK is installed
+
+if ! command -v dotnet &> /dev/null
+then
+    echo ".NET SDK could not be found. Please install it from https://dotnet.microsoft.com/download"
+    exit 1
+fi
+
+echo " ███████╗███████╗██████╗ ██╗   ██╗██╗   ██╗███████╗
+ ██╔════╝██╔════╝██╔══██╗██║   ██║██║   ██║██╔════╝
+ █████╗  █████╗  ██████╔╝██║   ██║██║   ██║█████╗  
+ ██╔══╝  ██╔══╝  ██╔══██╗██║   ██║██║   ██║██╔══╝  
+ ███████╗███████╗██║  ██║╚██████╔╝╚██████╔╝███████╗
+ ╚══════╝╚══════╝╚═╝  ╚═╝ ╚═════╝  ╚═════╝ ╚══════╝
+                    E Z   B U S
+"
+
+branch=$(git rev-parse --abbrev-ref HEAD)
+version=$(git describe --tags)
+echo current branch $branch
+# Check if the current branch is 'main'
+if [ "$branch" != "main" ]; then
+    cc=$(git rev-list --count $branch)
+    version="$version-beta-$branch-$cc"
+fi
+
+echo "Building the project version $version..."
+dotnet restore "./EzBus.sln"
+dotnet build "./EzBus.sln" -c Release
+echo "Running tests..."
+dotnet test "./EzBus.sln" -c Release
+echo "Packing the project..."
+dotnet pack "./EzBus.Core/EzBus.Core.csproj" -c Release -p:Version=$version 
+dotnet pack "./EzBus.RabbitMQ/EzBus.RabbitMQ.csproj" -c Release  -p:Version=$version
